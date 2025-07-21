9edc17d63481b01a226b6d671b1b2227532e08d2
whtjdgh1931 <pmkjo1931@gmail.com>
2025-07-21 22:05:55 +0900

    feat: addListener

--- Assets/02_Scripts/04_UI/JoyStickPanel.cs
+++ Assets/02_Scripts/04_UI/JoyStickPanel.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JoyStickPanel : MonoBehaviour
 {
+    public Button attackBtn;
+    public Button skillBtn;
+    public Button ultiBtn;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player_Attack playerAttack = GameObject.Find(Constants.NAME_Player).GetComponent<Player_Attack>();
+        attackBtn.onClick.AddListener(playerAttack.PressAtkBtn);
+        skillBtn.onClick.AddListener(playerAttack.PressSkillBtn);
+        ultiBtn.onClick.AddListener(playerAttack.PressUltiBtn);
     }
 
     // Update is called once per frame
