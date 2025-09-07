a1f36990b77fe9ba407ba5c0c9d683abd425aeee
whtjdgh1931 <pmkjo1931@gmail.com>
2025-09-08 00:07:28 +0900

    feat : camera

--- Assets/02_Scripts/02_Soldier/SoldierAnim.cs
+++ Assets/02_Scripts/02_Soldier/SoldierAnim.cs
@@ -4,39 +4,18 @@ using UnityEngine.AI;
 public class SoldierAnim : MonoBehaviour
 {
     private NavMeshAgent agent;
-    private SpriteRenderer[] spriteRenderers;
     private Animator anim;
     private FSM FSM;
-    private bool isFlip;
 
     private void Awake()
     {
         agent = GetComponentInParent<NavMeshAgent>();
-        spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
         anim = GetComponent<Animator>();
         FSM = GetComponentInParent<FSM>();
-        isFlip = spriteRenderers[0].flipX;
 
     }
 
-    private void Update()
-    {
-        if ((FSM.targetSoldier != null))
-        {
-            float tempX = FSM.targetSoldier.transform.position.x - transform.position.x;
-            foreach (SpriteRenderer animRenderer in spriteRenderers)
-            {
-                if (tempX < 0)
-                    animRenderer.flipX = !isFlip;
-                else animRenderer.flipX = isFlip;
-                //animRenderer.transform.forward = Camera.main.transform.forward;
-
-            }
-
-        }
-
-    }
-
+    
     public void SetAnimAttack()
     {
         anim.SetTrigger("IsAttack");
