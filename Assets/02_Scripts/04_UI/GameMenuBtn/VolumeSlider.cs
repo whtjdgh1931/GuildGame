918019fa47355c4b91685c254c079addef9bcdff
whtjdgh1931 <whtjdgh1931@naver.com>
2025-11-18 16:43:19 +0900

    feature : 씬 추가

--- Assets/02_Scripts/04_UI/GameMenuBtn/VolumeSlider.cs
+++ Assets/02_Scripts/04_UI/GameMenuBtn/VolumeSlider.cs
@@ -1,17 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class VolumeSlider : MonoBehaviour
 {
     public Slider volumeSlider;
-
+	[SerializeField] private TextMeshProUGUI _maxVolume;
+	[SerializeField] private TextMeshProUGUI _currentVolume;
 
 		public void Start()
 		{
 				volumeSlider = GetComponent<Slider>();
 				volumeSlider.onValueChanged.AddListener(VolumeManager.Instance().SetVolume);
-				
+		volumeSlider.onValueChanged.AddListener(SetVolumeText);
+		_maxVolume.text = volumeSlider.maxValue.ToString();		
 		}
+
+	public void SetVolumeText(float currentVolume)
+	{
+		_currentVolume.text = currentVolume.ToString();
+	}
+
+	
+
 }
